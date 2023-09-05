272f9bffd59709230a1f40d51a619c6cb6f2ed94
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-05 02:40:57 -0300

    Add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        '#161617': '#161617',
+        '#1c1c1c': '#1c1c1c',
+        '#5c5c5c': '#5c5c5c',
+        '#d4d4d4': '#d4d4d4',
+        '#dbdbdb': '#dbdbdb',
+        '#262729': '#262729',
+        '#4c4e54': '#4c4e54',
+        '#1f1f1f': '#1f1f1f',
+        '#00609c': '#00609c',
+        '#22a4f5' : '#22a4f5'
+      },
     },
   },
   plugins: [],
