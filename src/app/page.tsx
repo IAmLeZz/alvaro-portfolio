3c9fa913f798dda1011977f540ccdbc34a3069e8
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-07 03:24:33 -0300

    Add technologies component and its h3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import AboutMe from '@/components/AboutMe'
+import { Technologies } from '@/components/Technologies'
+import 'hover.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ zIndex: '1' }}>
-      <h2 className='lg:text-[6em] text-[2em] text-j text-gray-200 welcome'>Bienvenidos a mi portafolio<span className='text-#22a4f5'>.</span></h2>
+    <main className='flex flex-col items-center justify-between' style={{ zIndex: '1' }}>
+      <h2 className='lg:text-[6em] text-[1.7em] text-gray-200 text-center welcome'>Bienvenidos a mi portafolio<span className='text-#22a4f5'>.</span></h2>
       <AboutMe></AboutMe>
+      <h3 className='lg:text-[4em] text-[2em] text-center flex m-auto text-gray-200 technologies hvr hvr-grow'>Tecnologías que domino</h3>
+      <Technologies></Technologies>
     </main>
   )
 }
