39fae18975b747e72668b25e0dc832c595210178
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-12 04:49:47 -0300

    Add antialiased to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import 'hover.css'
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between' style={{ zIndex: '1' }}>
-      <h2 className='lg:text-[6em] text-[1.7em] text-gray-200 text-center welcome'>Bienvenidos a mi portafolio<span className='text-#22a4f5'>.</span></h2>
+    <main className='flex flex-col items-center justify-between antialiased' style={{ zIndex: '1' }}>
+      <h2 className='lg:text-[6em] text-[1.7em] text-gray-200 text-center welcome'>Bienvenidos a mi portfolio<span className='text-#22a4f5'>.</span></h2>
       <AboutMe></AboutMe>
       <h3 className='lg:text-[4em] text-[2em] text-center flex m-auto text-gray-200 technologies hvr hvr-grow'>Tecnologías que domino</h3>
       <Technologies></Technologies>
