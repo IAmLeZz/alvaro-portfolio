6b7db7af0b1aafb8267ba5b65126df099e22116a
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-13 02:51:36 -0300

    Remove default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import ParticlesBackground from '@/components/ParticlesBackground'
 import { Footer } from '@/components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Álvaro Villaló Portfolio',
@@ -19,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ParticlesBackground />
         <Navbar />
         {children}
