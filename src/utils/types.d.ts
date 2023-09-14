11186f2b5819ef8d061db2278bde17ce61151272
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-14 03:15:15 -0300

    Add types for Project

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,4 +1,5 @@
 interface Technology {
+    id: number;
     name: string;
     image: string;
 }
@@ -7,4 +8,24 @@ interface PersonalInfo {
     description: string;
     image: string;
     resume: string;
+}
+
+interface Media {
+    id: number;
+    caption?: string;
+    url?: string;
+    type: string;
+}
+
+interface Project {
+    id: number;
+    name: string;
+    client?: string;
+    category?: string;
+    description: string;   
+    thumbnail: string;
+    link?: string;
+    repository?: string;
+    technologies: Technology[];
+    media: Media[];
 }
