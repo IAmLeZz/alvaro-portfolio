1d57d6543cf6e63b250f9a9a9dede2b394f7f824
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-17 04:43:07 -0300

    Change language to spanish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         <ParticlesBackground />
         <Navbar />
