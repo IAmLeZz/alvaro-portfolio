441e1ad125ae21d8fdf4ca8699128270ef6667c8
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-17 04:47:57 -0300

    Fix styles and add Powered by Next.js text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,7 @@ export const Footer = () => {
     return (
         <footer className="bg-gray-900">
             <div className="container px-6 py-12 mx-auto">
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
-
+                <div className="grid grid-cols-1 relative z-10 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
                     <div>
                         <p className="font-semibold text-gray-800 dark:text-white">Acceso rápido</p>
 
@@ -31,6 +30,7 @@ export const Footer = () => {
                 <hr className="my-6 border-gray-200 md:my-8 dark:border-gray-700" />
 
                 <div className="flex items-center justify-between">
+                    <p className='font-semibold text-[1em] text-gray-300'>Powered by Next.js.</p>
                     <p className='text-center font-bold text-[1em] text-gray-200'>Copyright © 2023 <a href='/'>Álvaro Roberto Villaló</a></p>
                 </div>
             </div>
