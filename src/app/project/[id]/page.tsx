04f40c357a5d95c8c0e5943cc5a21337eb3f7dd2
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-17 04:48:07 -0300

    Add page.tsx for the Project page

---
+++ src/app/project/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PageProps } from '../../../../.next/types/app/layout';
+import { Project } from './Project';
+
+export default function page({ params }: PageProps) {
+    return (
+        <main className='z-10 antialiased'>
+            <Project params={params}></Project>
+        </main>
+    )
+}
