8fcf7a0307448a19b9328f3372ec4a7d9f9bddcd
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-09-17 22:33:24 -0300

    Add hrefs to the buttons. Add shadows

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
 
 export const Footer = () => {
     return (
-        <footer className="bg-gray-900">
-            <div className="container px-6 py-12 mx-auto">
-                <div className="grid grid-cols-1 relative z-10 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+        <footer className="bg-gray-900 shadow-xl shadow-blue-200">
+            <div className="container px-6 py-12 mx-auto shadow-lg shadow-blue-900">
+                <div className="grid grid-cols-1 relative z-10 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4 ">
                     <div>
                         <p className="font-semibold text-gray-800 dark:text-white">Acceso rápido</p>
 
                         <div className="flex flex-col items-start mt-5 space-y-2">
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Sobre mi</a>
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Tecnologías</a>
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Proyectos y experiencia</a>
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Contacto</a>
+                            <a href="#aboutme" target='_blank' className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Sobre mi</a>
+                            <a href="#technologies" target='_blank' className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Tecnologías</a>
+                            <a href="#projects" target='_blank' className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Proyectos y experiencia</a>
+                            <a href="#contact" target='_blank' className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Contacto</a>
                         </div>
                     </div>
 
@@ -20,9 +21,13 @@ export const Footer = () => {
                         <p className="font-semibold text-gray-800 dark:text-white">Redes sociales</p>
 
                         <div className="flex flex-col items-start mt-5 space-y-2">
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">LinkedIn</a>
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">GitHub</a>
-                            <a href="#" className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Twitter</a>
+                            <a href="https://www.linkedin.com/in/alvaro-villalo-web-developer/" target='_blank' className="text-gray-600 flex items-center transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">
+                                LinkedIn <AiOutlineLinkedin style={{ marginLeft: '0.2rem' }}></AiOutlineLinkedin>
+                            </a>
+                            <a href="https://github.com/IAmLeZz" target='_blank' className="text-gray-600 flex items-center transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">
+                                GitHub <AiOutlineGithub style={{ marginLeft: '0.2rem' }}></AiOutlineGithub>
+                            </a>
+                            <a href="#" target='_blank' className="text-gray-600 flex items-center transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:text-blue-500">Twitter</a>
                         </div>
                     </div>
                 </div>
