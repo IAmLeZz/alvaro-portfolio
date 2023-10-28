fcf0f6776d0f8dedda8a42b19aedabc26da05d0a
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-10-27 23:36:13 -0300

    Add a new sticky effect when scrolling down.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,63 @@
-
+"use client"
+import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
+    const [isFixed, setIsFixed] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+            setIsFixed(true);
+        } else {
+            setIsFixed(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const navbarClass = isFixed ? 'navbar bg-gray-900 text-white py-1 md:py-3 w-full shadow-md shadow-blue-900 z-[100] fixed' : 'navbar bg-gray-900 text-white py-3 md:py-6 w-full shadow-md shadow-blue-900 z-[100] relative';
+
     return (
-        <nav className="bg-gray-900 text-white py-4 w-full relative shadow-md shadow-blue-900">
-            <div className="container mx-auto flex justify-between items-center">
-                <ul className="flex space-x-6 z-10">
-                    <li>
-                        <Link href="/" className="text-gray-400 hover:text-white font-bold">
-                            INICIO
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/#aboutme" className="text-gray-400 hover:text-white font-bold">
-                            SOBRE MI
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/#projects" className="text-gray-400 hover:text-white font-bold">
-                            PROYECTOS
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/#contact" className="text-gray-400 hover:text-white font-bold">
-                            CONTACTO
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav className={navbarClass}>
+                <div className="container mx-auto flex justify-between items-center">
+                    <div className="flex items-center">
+                        <div className="mr-4">
+                            <Image src={'/../../logo.png'} alt='Logo' width={30} height={30} className='flex items-center justify-center'></Image>
+                        </div>
+                        <ul className="flex space-x-6 z-10">
+                            <li>
+                                <Link href="/" className="text-gray-400 hover:text-white font-bold">
+                                    INICIO
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/#aboutme" className="text-gray-400 hover:text-white font-bold">
+                                    SOBRE MI
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/#projects" className="text-gray-400 hover:text-white font-bold">
+                                    PROYECTOS
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/#contact" className="text-gray-400 hover:text-white font-bold">
+                                    CONTACTO
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div style={{ height: isFixed ? '80px' : '0px' }}></div>
+        </>
     );
 };
-
